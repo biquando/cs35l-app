6027c3cba1e85641b0c05d2fe3c78770794a5af4
omerdemirkan <omerfarukpiano@gmail.com>
2022-01-23 08:54:20 -0800

    create dedicated dev and production scripts

--- api/src/config.js
+++ api/src/config.js
@@ -2,7 +2,10 @@ require("dotenv").config();
 
 const config = {
   PORT: 8080,
-  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_URI:
+    process.env.NODE_ENV === "development"
+      ? process.env.MONGODB_LOCAL_URI
+      : process.env.MONGODB_ATLAS_URI,
   SALT_ROUNDS: +process.env.SALT_ROUNDS,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
