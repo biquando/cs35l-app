54769fd9f8535c6db976776eb75170e21a4494d5
omerdemirkan <omerfarukpiano@gmail.com>
2022-01-23 09:10:15 -0800

    create login endpoint

--- api/src/routes/auth/auth.controllers.js
+++ api/src/routes/auth/auth.controllers.js
@@ -19,3 +19,19 @@ module.exports.signUp = async function (req, res) {
       .send({ error: error.message, details: JSON.stringify(error) });
   }
 };
+
+module.exports.login = async function (req, res) {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password_hash)))
+      throw new Error("Incorrect username or password");
+    const token = jwt.sign({ user_id: user._id }, config.ACCESS_TOKEN_SECRET);
+    res.send({ token });
+  } catch (error) {
+    res.status(400).send({
+      error: error.message,
+      details: JSON.stringify(error),
+    });
+  }
+};
--- api/src/routes/auth/auth.routes.js
+++ api/src/routes/auth/auth.routes.js
@@ -4,5 +4,6 @@ const authControllers = require("./auth.controllers");
 const authRouter = Router();
 
 authRouter.post("/sign-up", authControllers.signUp);
+authRouter.post("/login", authControllers.login);
 
 module.exports = authRouter;
