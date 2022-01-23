729b0ddea45e3598cebd7b30e11ae79328d511e2
omerdemirkan <omerfarukpiano@gmail.com>
2022-01-22 19:56:27 -0800

    fix silly port discrepency

--- api/src/index.js
+++ api/src/index.js
@@ -5,7 +5,7 @@ const config = require("./config");
 async function main() {
   const app = express();
   await configureApp(app);
-  await app.listen(3000, () =>
+  await app.listen(config.PORT, () =>
     console.log(`Server up and running on port ${config.PORT}`)
   );
 }
