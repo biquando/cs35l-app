1cf814f9fc850af9ea9e00ce174b29f19913509e
omerdemirkan <omerfarukpiano@gmail.com>
2022-02-05 15:02:33 -0800

    Add cors middleware to backend

--- api/src/loaders/express.loader.js
+++ api/src/loaders/express.loader.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 
 async function expressLoader(app) {
+  app.use(cors());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 }
