c4006a1cbd99bf90717b54f6ed254b0e2163e289
PitapatCat <kevwjin@gmail.com>
2022-02-06 09:51:44 -0800

    Add router to link login and sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
-import LoginForm from "./components/LoginForm/LoginForm";
-// import "./scss/custom.scss";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <LoginForm />;
+  return (
+    <Router>
+      <nav>
+        <Link to="/"> Login </Link>
+        <Link to="/about"> SignUp </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <div> Footer </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,80 @@
+import React from "react";
+// import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import "../styles/signup.css";
+import { Link } from "react-router-dom";
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      msg: null,
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  Login = () => {
+    console.log("login");
+  };
+
+  Logout = () => {
+    console.log("Logout");
+  };
+
+  onChange = () => {
+    console.log("change");
+  };
+
+  onSubmit = () => {
+    console.log("submit");
+  };
+
+  render() {
+    return (
+      <div className="center text-center">
+        <div className="inner-container">
+          <div className="login-card">
+            <h1 className="font">{"Bubble"}</h1>
+            <form onSubmit={this.onSubmit} className="form-signin">
+              <input
+                name="username"
+                type="text"
+                value={this.state.username}
+                onChange={this.onChange}
+                placeholder="Username"
+                autoFocus
+                className="form-control"
+              />
+              <input
+                name="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.onChange}
+                placeholder="Password"
+                className="form-control"
+              />
+              <button
+                className="button btn btn-lg btn-primary btn-block"
+                disabled={this.props.loadingSubmit}
+              >
+                {this.props.loadingSubmit ? (
+                  <span className="spinner-border spinner-border-sm" />
+                ) : (
+                  "Login"
+                )}
+              </button>
+            </form>
+            {/* TODO: register link */}
+            <p>
+              <Link to="/signup">Sign Up</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
