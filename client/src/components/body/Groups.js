d2eb7430a5449432d7cb37495843c33f9b1e0da5
kevwjin <kevwjin@gmail.com>
2022-03-05 13:24:17 -0800

    Disable submit button when logging in

--- client/src/components/body/Groups.js
+++ client/src/components/body/Groups.js
@@ -3,7 +3,40 @@ import { Link } from "react-router-dom";
 import "../../styles/groups.css";
 import "../../styles/body.css";
 
-function Sidebar(props) {
+function Groups(props) {
+  const groupData = [
+    {
+      events: [
+        {
+          eventname: "name1",
+          date: "date1",
+          description: "description1",
+        },
+      ],
+      comments: [
+        {
+          username: "username1",
+          text: "this is a nice comment",
+        },
+        {
+          username: "username2",
+          text: "this is an ok comment",
+        },
+        {
+          username: "username1",
+          text: "this is a detailed comment",
+        },
+        {
+          username: "username2",
+          text: "this is a mean comment",
+        },
+        {
+          username: "username3",
+          text: "this is a sad comment",
+        },
+      ],
+    },
+  ];
   return (
     <div className="parent-container">
       <div className="content-container">
@@ -26,7 +59,7 @@ function Sidebar(props) {
           <span className="group-title text-light">group</span>
         </Link>
         <Link to="/" className="btn-secondary group-box">
-          <span className="group-title text-light">group</span>
+          <span className="group-title text-light">groupasdfasdfsadf</span>
         </Link>
         <Link to="/" className="btn-secondary group-box">
           <span className="group-title text-light">group</span>
@@ -62,4 +95,4 @@ function Sidebar(props) {
   );
 }
 
-export default Sidebar;
+export default Groups;
