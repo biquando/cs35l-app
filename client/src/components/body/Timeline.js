9b66d80659a8f0dff3aca9c0cb3a085325af896e
omerdemirkan <omerfarukpiano@gmail.com>
2022-03-05 17:30:44 -0800

    Add :groupId param to create-event route

--- client/src/components/body/Timeline.js
+++ client/src/components/body/Timeline.js
@@ -1,15 +1,16 @@
 // import "./App.css";
-import React from "react";
+import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/timeline.css";
 import "../../styles/body.css";
+import { format, isSameDay } from "date-fns";
 
 function Day(props) {
   return (
     <div>
       <div>
         <div>
-          <h6 className="">{props.date}</h6>
+          <h6 className="">{format(props.date, "d LLLL, y")}</h6>
           <ul className="list-group">
             {props.events.map((event) => (
               <li className="list-group-item shadow-realm">
@@ -25,93 +26,60 @@ function Day(props) {
   );
 }
 
-class Timeline extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [
-        {
-          date: "07 Feb Monday",
-          events: [
-            {
-              title: "Assignment 1",
-              description: "This is the first assignment.",
-            },
-            {
-              title: "Assignment 2",
-              description: "This is the second assignment.",
-            },
-          ],
-        },
-        {
-          date: "08 Feb Tuesday",
-          events: [
-            {
-              title: "Assignment 3",
-              description: "This is the third assignment.",
-            },
-          ],
-        },
-        {
-          date: "10 Feb Thursday",
-          events: [
-            {
-              title: "Midterm",
-            },
-          ],
-        },
-        {
-          date: "07 Feb Monday",
-          events: [
-            {
-              title: "Assignment 1",
-              description: "This is the first assignment.",
-            },
-            {
-              title: "Assignment 2",
-              description: "This is the second assignment.",
-            },
-          ],
-        },
-        {
-          date: "08 Feb Tuesday",
-          events: [
-            {
-              title: "Assignment 3",
-              description: "This is the third assignment.",
-            },
-          ],
-        },
-        {
-          date: "10 Feb Thursday",
-          events: [
-            {
-              title: "Midterm",
-            },
-          ],
-        },
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <div className="parent-container">
-        <div className="content-container">
-          {this.state.list.map((day) => (
-            <Day date={day.date} events={day.events} />
-          ))}
-        </div>
-        <div className="sticky-bot">
-          <Link to="/create-event">
+function Timeline({ events, loading, selectedGroup }) {
+  const days = useMemo(() => getDays(events), [events]);
+  return (
+    <div className="parent-container">
+      <div className="content-container">
+        {days?.length ? (
+          days.map((day) => (
+            <Day
+              key={day.date.toString()}
+              date={day.date}
+              events={day.events}
+            />
+          ))
+        ) : (
+          <ul className="list-group">
+            <li className="list-group-item shadow-realm">
+              <b>{loading ? "Loading..." : "No events created"}</b>
+            </li>
+          </ul>
+        )}
+      </div>
+      <div className="sticky-bot">
+        {selectedGroup ? (
+          <Link to={`/group/${selectedGroup._id}/create-event`}>
             <button className="button btn btn-md btn-primary btn-block">
               Create Event
             </button>
           </Link>
-        </div>
+        ) : (
+          <button disabled className="button btn btn-md btn-primary btn-block">
+            Create Event
+          </button>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Timeline;
+
+function getDays(events) {
+  if (!events?.length) return [];
+  const days = [];
+  events = [...events].sort(
+    (a, b) => new Date(a.end_date).getTime() - new Date(b.end_date).getTime()
+  );
+  events.forEach((event, i) => {
+    if (
+      i > 0 &&
+      isSameDay(new Date(event.end_date), new Date(events[i - 1].end_date))
+    ) {
+      days[days.length - 1].events.push(event);
+    } else {
+      days.push({ date: new Date(event.end_date), events: [event] });
+    }
+  });
+}
