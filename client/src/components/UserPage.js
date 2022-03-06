56416bc6f69a2976414cd5455d941938b1413eed
Quan Do <biquando@gmail.com>
2022-03-05 19:50:34 -0800

    Implement leaving and deleting groups

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -3,19 +3,30 @@ import "../styles/userpage.css";
 import { useParams } from "react-router-dom";
 import { useUser, useGroups } from "../utils/swr";
 import { useAuth } from "../contexts/AuthContext";
+import { leaveGroup, deleteGroup } from "../utils/group";
 import NavBar from "./NavBar";
 
 function UserPage(props) {
   const userId = useParams().user_id;
   const { data: user } = useUser({ userId });
-  const { data: groups, isValidating: isValidatingGroups } = useGroups(
-    { userIds: [user?._id] },
-    !!user
-  );
+  const {
+    data: groups,
+    isValidating: isValidatingGroups,
+    mutate: mutateGroups,
+  } = useGroups({ userIds: [user?._id] }, !!user);
+  const isGroupsLoading = !groups && isValidatingGroups;
 
   const { user: currentUser } = useAuth();
 
-  const isGroupsLoading = !groups && isValidatingGroups;
+  async function handleLeaveGroup(groupId) {
+    await leaveGroup({ groupId });
+    mutateGroups();
+  }
+
+  async function handleDeleteGroup(groupId) {
+    await deleteGroup({ groupId });
+    mutateGroups();
+  }
 
   return (
     <div>
@@ -25,7 +36,7 @@ function UserPage(props) {
           <div className="content-container">
             <h1>{user?.username}</h1>
             <p>{user?.description}</p>
-            <h4>Groups:</h4>
+            {groups?.length > 0 && <h4>Groups:</h4>}
             {isGroupsLoading ? null : (
               <ul className="list-group">
                 {groups?.map((group) => (
@@ -33,11 +44,17 @@ function UserPage(props) {
                     <div>{group.name}</div>
                     {user?._id == currentUser?._id &&
                       (user?._id != group.owner_id ? (
-                        <button className="badge bg-secondary rounded-pill">
+                        <button
+                          className="badge bg-secondary rounded-pill"
+                          onClick={() => handleLeaveGroup(group._id)}
+                        >
                           Leave
                         </button>
                       ) : (
-                        <button className="badge bg-danger rounded-pill">
+                        <button
+                          className="badge bg-danger rounded-pill"
+                          onClick={() => handleDeleteGroup(group._id)}
+                        >
                           Delete
                         </button>
                       ))}
