fb67d4de3d60ab7421859fb167af40edfedb5be4
Quan Do <biquando@gmail.com>
2022-03-05 19:30:39 -0800

    Render buttons to leave or delete groups

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../styles/userpage.css";
 import { useParams } from "react-router-dom";
 import { useUser, useGroups } from "../utils/swr";
+import { useAuth } from "../contexts/AuthContext";
 import NavBar from "./NavBar";
 
 function UserPage(props) {
@@ -11,6 +12,8 @@ function UserPage(props) {
     { userIds: [user?._id] },
     !!user
   );
+
+  const { user: currentUser } = useAuth();
 
   const isGroupsLoading = !groups && isValidatingGroups;
 
@@ -28,7 +31,16 @@ function UserPage(props) {
                 {groups?.map((group) => (
                   <li className="list-group-item d-flex justify-content-between align-items-start">
                     <div>{group.name}</div>
-                    <button className="badge bg-danger rounded-pill">X</button>
+                    {user?._id == currentUser?._id &&
+                      (user?._id != group.owner_id ? (
+                        <button className="badge bg-secondary rounded-pill">
+                          Leave
+                        </button>
+                      ) : (
+                        <button className="badge bg-danger rounded-pill">
+                          Delete
+                        </button>
+                      ))}
                   </li>
                 ))}
               </ul>
