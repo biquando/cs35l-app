217f3fc9b7fe232c0a631ce437bb261a144fd9ab
mtsang38 <mjtsang88@gmail.com>
2022-03-06 17:51:50 -0800

    Partially functional comments

--- client/src/components/body/Comments.js
+++ client/src/components/body/Comments.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useUser } from "../../utils/swr.js";
+import { createMessage } from "../../utils/message.js"
 import "../../styles/comments.css";
 import "../../styles/body.css";
 
 function Comment(props) {
+  const {data: user } = useUser({userId: props.userId})
   return (
     <div>
       <div className="">
         <div className="">
-          <b>{props.username}</b>
+          <b>{user.username}</b>
           <p>{props.text}</p>
         </div>
       </div>
@@ -16,128 +20,63 @@ function Comment(props) {
   );
 }
 
+
+
 function Comments(props) {
+
+  const[state,setState] = useState({
+    groupId:"",
+    eventId:"",
+    text: "",
+
+  });
+  const [loading, setLoading] = React.useState(false)
   const comments = [
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
-    {
-      username: "Assignment 1",
-      text: "This is the first assignment.",
-    },
-    {
-      username: "Assignment 2",
-      text: "This is the second assignment.",
-    },
   ];
 
-  const handleComment = () => {
-    return;
+
+
+  const handleComment = (e) => {
+    const { groupId, eventId, value: text } = e.target;
+    console.log(text);
+    setState(prev => ({...prev, text}))
   };
+
+  const submitCommentLine = async (e) => {
+    console.log("commented");
+
+    e.preventDefault();
+    const { groupId, eventId, text } = state;
+    setLoading(true)
+    await createMessage({ groupId, eventId, text });
+    setLoading(false)
+
+  };
+
 
   return (
     <div className="parent-container expand">
       <div className="content-container">
-        {comments.map((comment) => (
-          <Comment username={comment.username} text={comment.text} />
+        {props.messages?.map((comment) => (
+          <Comment userId={comment.user_id} text={comment.text} />
         ))}
       </div>
       <div className="sticky-bot">
         <input
           name="username"
           type="comment"
-          value={comments.text}
+          value={state.text}
           onChange={handleComment}
           placeholder="Add a comment..."
           autoFocus
           className="comment-input"
         />
       </div>
+
+      <button onClick = {submitCommentLine} type="submit"
+      className="comment-button"
+      disabled={loading}
+      >Post</button>
     </div>
   );
 }
