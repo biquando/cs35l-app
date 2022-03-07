b58226fc5b0a28b3991ec60bf9a55c087f957632
kevwjin <kevwjin@gmail.com>
2022-03-06 17:16:03 -0800

    Add modal for add group button
    The modal contains buttons that link to the create group and
    join group pages.

--- client/src/components/body/AddGroupCard.js
+++ client/src/components/body/AddGroupCard.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/profile.css";
+import "../../styles/addgroupcard.css";
 
 function AddGroupCard(props) {
   return (
-    <div className="profile-card">
+    <div className="add-group-card">
       <Link to="/create-group" className="username link-dark">
         <button className="logout-btn">
-          <div className="logout-text">Log Out</div>
+          <div className="logout-text">Create Group</div>
         </button>
       </Link>
       <Link to="/join-group" className="username link-dark">
         <button className="logout-btn">
-          <div className="logout-text">Log Out</div>
+          <div className="logout-text">Join Group</div>
         </button>
       </Link>
     </div>
